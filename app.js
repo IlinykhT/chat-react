6b4ca6a69bc5c8c1e07182509ea3c3c1e61e0005
IlinykhT <IlinykhTa999@gmail.com>
2018-10-04 16:05:14 +0700

    adding files

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
 class App extends React.Component {
-	
 	constructor() {
 		super();
 		if (!localStorage.getItem("chat")) {
 			localStorage.setItem("chat",JSON.stringify([]));
 		}	
-		var chatHistory = JSON.parse(localStorage.getItem("chat"));
+		const chatHistory = JSON.parse(localStorage.getItem("chat"));
 
 		this.state = {
 		   message: chatHistory
 		};
         this.sendMessage = this.sendMessage.bind(this);
 	}
-	
-    sendMessage(oneMess) {
-		var messArray=this.state.message;
+    
+	sendMessage(oneMess) {
+		let messArray = this.state.message;
 		messArray.push(oneMess);
 		this.setState(messArray);
 		localStorage.setItem("chat",JSON.stringify(this.state.message));
     }	
-		
+
 	render() {
 		return (
-			<div className="container">
+			<div className = "container">
 				<Title />
-				<MessageList data={this.state.message}/>
-				<SendMessageForm sendMessage={this.sendMessage} />
+				<MessageList data = {this.state.message}/>
+				<SendMessageForm sendMessage = {this.sendMessage} />
 			</div>
 		);
 	}
 }
 
-
 class MessageList extends React.Component {
   render() {
     return (
-		<div className="chatarea">
+		<div className = "chatarea">
 			{this.props.data.map((message, index) => {
 				return (
-					<div key={index}>
+					<div key = {index}>
 					   <strong>{message.nick}: </strong>{message.mess}
 					</div> 
 				)})
@@ -49,17 +47,17 @@ class MessageList extends React.Component {
 }
 
 function Title() {
-  return <div className='head'>Enjoy chatting!</div>
+  return <div className="head">Enjoy chatting!</div>
 }
 
 class SendMessageForm extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
-			nick: '',
-            mess: ''
+			nick: "",
+            mess: ""
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChangeNick = this.handleChangeNick.bind(this);
 		this.handleChangeMess = this.handleChangeMess.bind(this);
     }
@@ -68,8 +66,7 @@ class SendMessageForm extends React.Component {
 		e.preventDefault();
         this.props.sendMessage(this.state);
         this.setState({
-			nick: '',
-            mess: ''
+            mess: ""
         })
     }
 	
@@ -87,24 +84,22 @@ class SendMessageForm extends React.Component {
     
     render() {
         return (
-		<div className="sending">
-			<form onSubmit={this.handleSubmit}>
-				<div className="input-block">
-					<label>Nickname</label>
-					<input id="nickInput" type='text' value={this.state.nick} onChange={this.handleChangeNick} /> 
-					<br/><br/>
-					<label>Message</label>
-					<input id="messageInput" type='text' value={this.state.mess} onChange={this.handleChangeMess} /> 
-				</div>
-				<div className="input-block">
-					<input type="submit" className="send-button" value="Send"/>
-				</div>
-			</form>	
-		</div>	
+			<div className="sending">
+				<form onSubmit={this.handleSubmit}>
+					<div className="input-block">
+						<label>Nickname</label>
+						<input id="nickInput" type="text" value={this.state.nick} onChange={this.handleChangeNick} /> 
+						<br/><br/>
+						<label>Message</label>
+						<input id="messageInput" type="text" value={this.state.mess} onChange={this.handleChangeMess} /> 
+					</div>
+					<div className="input-block">
+						<input type="submit" className="send-button" value="Send"/>
+					</div>
+				</form>	
+			</div>	
         )
     }
 }
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
