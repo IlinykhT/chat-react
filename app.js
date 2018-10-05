53d7c1fc4ef1eaa16292f267893b354d896b46d4
IlinykhT <IlinykhTa999@gmail.com>
2018-10-05 14:31:17 +0700

    adding files

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ class App extends React.Component {
 		if (!localStorage.getItem("chat")) {
 			localStorage.setItem("chat",JSON.stringify([]));
 		}	
-		const chatHistory = JSON.parse(localStorage.getItem("chat"));
-
 		this.state = {
-			message: chatHistory
+			message: JSON.parse(localStorage.getItem("chat"))
 		};
         this.sendMessage = this.sendMessage.bind(this);
 	}
     
 	sendMessage(oneMess) {
 		let messArray = this.state.message;
-		messArray.push(oneMess);
+		let obj = {nick: oneMess.nick, mess: oneMess.mess};
+		messArray.push(obj);
 		this.setState(messArray);
-		localStorage.setItem("chat",JSON.stringify(this.state.message));
+		localStorage.setItem("chat", JSON.stringify(this.state.message));
+		
     }	
 
 	render() {
@@ -31,6 +31,14 @@ class App extends React.Component {
 }
 
 class MessageList extends React.Component {
+	componentDidMount() {
+		this.messagesEnd.scrollIntoView();
+	}
+	
+	componentDidUpdate() {
+		this.messagesEnd.scrollIntoView();
+	}	
+	
 	render() {
 		return (
 			<div className = "chatarea">
@@ -41,6 +49,7 @@ class MessageList extends React.Component {
 						</div> 
 					)})
 				}	
+				<div ref= {(div) => { this.messagesEnd = div; }}></div>
 			</div>
 		)
 	}
@@ -55,46 +64,65 @@ class SendMessageForm extends React.Component {
         super();
         this.state = {
 			nick: "",
-            mess: ""
+			mess: "",
+			submitDisabled: true
         }
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChangeNick = this.handleChangeNick.bind(this);
 		this.handleChangeMess = this.handleChangeMess.bind(this);
-    }
-    
+	}
+
     handleSubmit(e) {
 		e.preventDefault();
         this.props.sendMessage(this.state);
         this.setState({
-            mess: ""
-        })
+            mess: "",
+			submitDisabled: true
+        });
     }
 	
-	handleChangeNick(e) {
+	handleChangeNick(e) { 
+		let disabled = e.target.value && this.state.mess ? false : true;
 		this.setState({
-			nick: e.target.value
-		})
+			nick: e.target.value,
+			submitDisabled: disabled
+		});
     }
 	
 	handleChangeMess(e) {
+		let disabled = this.state.nick && e.target.value ? false : true;		
 		this.setState({
-			mess: e.target.value
-		})
+			mess: e.target.value,
+			submitDisabled: disabled
+		});
     }
-    
+	  
     render() {
         return (
 			<div className="sending">
 				<form onSubmit={this.handleSubmit}>
 					<div className="input-block">
 						<label>Nickname</label>
-						<input id="nickInput" type="text" value={this.state.nick} onChange={this.handleChangeNick} /> 
+						<input 
+							type="text" 
+							value={this.state.nick} 
+							onChange={this.handleChangeNick} 
+							autoComplete="off" 
+							className="nickInput" /> 
 						<br/><br/>
 						<label>Message</label>
-						<input id="messageInput" type="text" value={this.state.mess} onChange={this.handleChangeMess} /> 
+						<input 
+							type="text" 
+							value={this.state.mess} 
+							onChange={this.handleChangeMess} 
+							autoComplete="off" /> 
 					</div>
 					<div className="input-block">
-						<input type="submit" className="send-button" value="Send"/>
+						<input 
+							type="submit" 
+							className="send-button" 
+							value="Send" 
+							disabled={this.state.submitDisabled} />
 					</div>
 				</form>	
 			</div>	
